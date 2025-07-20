b5437b8016ab5580eea3db0839f38495bfa74b75
Grey Wind <wjunqin7@gmail.com>
2025-07-20 14:43:52 +0800

    update pages and cors

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,16 @@
 package routes
 
 import (
+	"NucleusAPI/middleware" // 添加这个导入
 	"github.com/gorilla/mux"
 )
 
 // NewRouter 创建并配置主路由器
 func NewRouter() *mux.Router {
 	router := mux.NewRouter().StrictSlash(true)
+
+	// 应用 CORS 中间件
+	router.Use(middleware.EnableCORS)
 
 	// 注册各类路由
 	RegisterBaseRoutes(router) // 基础路由
