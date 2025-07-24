65580932c1038dc38138577dccc75e4e01794c0b
Grey Wind <wjunqin7@gmail.com>
2025-07-24 12:46:35 +0800

    Route optimize (#7)
    * add routes log
    
    * support amd routes

--- routes/cpu_routes.go
+++ routes/cpu_routes.go
@@ -2,15 +2,18 @@ package routes
 
 import (
 	"NucleusAPI/handlers"
+	"fmt"
 
 	"github.com/gorilla/mux"
 )
 
 // RegisterCPURoutes 注册CPU相关路由（带限流）
 func RegisterCPURoutes(router *mux.Router) {
+	fmt.Printf("RegisterCPURoutes start\n")
 	cpuRouter := router.PathPrefix("/cpu").Subrouter()
 
 	cpuRouter.HandleFunc("/intel/{model}", handlers.GetIntelCPUInfo).Methods("GET")
 	cpuRouter.HandleFunc("/amd/{model}", handlers.GetAMDCPUInfo).Methods("GET")
 	cpuRouter.HandleFunc("/apple/{model}", handlers.GetAppleCPUInfo).Methods("GET")
+	fmt.Printf("RegisterCPURoutes end\n")
 }
