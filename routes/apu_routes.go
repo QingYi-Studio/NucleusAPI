75ac3019cf63d88fec11713055c3d8d41ea469fb
Grey Wind <wjunqin7@gmail.com>
2025-07-24 12:42:55 +0800

    add routes log

--- routes/apu_routes.go
+++ routes/apu_routes.go
@@ -2,14 +2,17 @@ package routes
 
 import (
 	"NucleusAPI/handlers"
+	"fmt"
 
 	"github.com/gorilla/mux"
 )
 
 // RegisterAPURoutes 注册APU相关路由（带限流）
 func RegisterAPURoutes(router *mux.Router) {
+	fmt.Printf("RegisterAPURoutes start\n")
 	apuRouter := router.PathPrefix("/apu").Subrouter()
 
 	apuRouter.HandleFunc("/amd/{model}", handlers.GetAMDAPUInfo).Methods("GET")
 	apuRouter.HandleFunc("/{model}", handlers.GetAMDAPUInfo).Methods("GET")
+	fmt.Printf("RegisterAPURoutes end\n")
 }
--- routes/base_routes.go
+++ routes/base_routes.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
@@ -18,6 +19,7 @@ var (
 
 // RegisterBaseRoutes 注册基础路由
 func RegisterBaseRoutes(router *mux.Router) {
+	fmt.Printf("RegisterBaseRoutes start\n")
 	// 根路径：返回 HTML 页面
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		serveHTML(w, filepath.Join(basePath, "pages", "index.html"))
@@ -27,6 +29,7 @@ func RegisterBaseRoutes(router *mux.Router) {
 	router.HandleFunc("/docs", func(w http.ResponseWriter, r *http.Request) {
 		serveHTML(w, filepath.Join(basePath, "pages", "docs.html"))
 	})
+	fmt.Printf("RegisterBaseRoutes end\n")
 }
 
 // serveHTML 辅助函数：提供HTML文件
